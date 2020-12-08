c35a64ee527c1e8ae68646765af6cc6e3d7f918d
mfraggy25 <mfragiskatos.github@gmail.com>
2020-12-07 23:33:02 -0500

    payment form and stripe

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -8,7 +8,7 @@ import PaymentForm from "../PaymentForm";
 
 const steps = ["Shipping address", "Payment details"];
 
-const Checkout = ({cart}) => {
+const Checkout = ({cart, order, onCaptureCheckout, error}) => {
     const [activeStep, setActiveStep] = useState(0);
     const [checkoutToken, setCheckoutToken] = useState(null);
     const [shippingData, setShippingData] = useState({});
@@ -46,7 +46,7 @@ useEffect(() => {
 
     const Form = () => activeStep === 0
         ? <AddressForm checkoutToken={checkoutToken} test={test} />
-        : <PaymentForm checkoutToken={checkoutToken} />
+        : <PaymentForm checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout}/>
 
 
     return (
